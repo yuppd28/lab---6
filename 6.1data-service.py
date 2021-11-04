cd25b58fcd43ce638b185df599d9151dcb26262f
Клюшніченко Дарина <90674467+yuppd28@users.noreply.github.com>
2021-11-04 22:24:41 +0200

    Add files via upload

---
+++ 6.1data-service.py
@@ -0,0 +1,9 @@
+import random 
+a=["American","Estonian","Latvian","German","Ukranian","Albanian","Italian","Serbian","Belarusian","Czech"]
+b=["Pole","Romanian","Russian","Canadian","Mexican","Bolivian","Colombian","Uruguayan","Iranian","Turk"]
+c=["friends","enemies","colleagues","patriots","partners","rivals","independent","understand each other","hate each other","companions"]
+s1=random.choice(a)
+s2=random.choice(b)
+s3=random.choice(c)
+
+print(s1+" and "+s2+" are "+s3)
